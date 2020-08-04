0cdad25ca6401273f9faa05b522776add440839d
cynthia_macias <cinthia.gomezmacias@gmail.com>
2020-08-03 22:30:00 -0500

    [ADD] Added "registration" and "login" screen layouts

--- app/src/main/java/com/example/torti_app_mobile/Activities/LoginActivity.java
+++ app/src/main/java/com/example/torti_app_mobile/Activities/LoginActivity.java
@@ -1,14 +1,16 @@
-package com.example.torti_app_mobile;
+package com.example.torti_app_mobile.Activities;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+import com.example.torti_app_mobile.R;
+
+public class LoginActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_login);
     }
 }
---
+++ app/src/main/java/com/example/torti_app_mobile/Activities/MainActivity.java
@@ -0,0 +1,16 @@
+package com.example.torti_app_mobile.Activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.torti_app_mobile.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
