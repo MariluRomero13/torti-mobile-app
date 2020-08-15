14b6f109767ba3ae80adc466e311984efae70466
marru <marrufojosecarlos@gmail.com>
2020-08-14 21:41:02 -0500

    Notification was added

--- app/src/main/java/com/example/torti_app_mobile/Activities/HomeActivity.java
+++ app/src/main/java/com/example/torti_app_mobile/Activities/HomeActivity.java
@@ -6,12 +6,22 @@ import androidx.appcompat.widget.Toolbar;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentTransaction;
 
+import android.app.Dialog;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.Toast;
 
+import com.android.volley.AuthFailureError;
+import com.android.volley.Request;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.example.torti_app_mobile.Classes.VolleyS;
 import com.example.torti_app_mobile.Framents.DelieveriesFragment;
 import com.example.torti_app_mobile.Framents.HistoryFragment;
 import com.example.torti_app_mobile.Framents.ProductsAssigmentFragment;
@@ -20,6 +30,12 @@ import com.example.torti_app_mobile.R;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import static com.example.torti_app_mobile.Classes.Enviroment.api_url;
 
 public class HomeActivity extends AppCompatActivity {
 
@@ -66,7 +82,7 @@ public class HomeActivity extends AppCompatActivity {
     public boolean onOptionsItemSelected(@NonNull MenuItem item) {
         switch (item.getItemId()) {
             case R.id.item_notify:
-                Toast.makeText(this, "Notificar", Toast.LENGTH_SHORT).show();
+                openDialogForNotification();
                 break;
             case R.id.item_reset_pass:
                 startActivity(new Intent(getApplicationContext(), ResetPasswordActivity.class));
@@ -82,6 +98,59 @@ public class HomeActivity extends AppCompatActivity {
                 break;
         }
         return super.onOptionsItemSelected(item);
+    }
+
+    private void openDialogForNotification() {
+        final Dialog dialog = new Dialog(this);
+        dialog.setContentView(R.layout.layout_dialog_notification);
+        final EditText edtNotification = dialog.findViewById(R.id.edtDescription);
+        final Button btnSendNotification = dialog.findViewById(R.id.btnSendNotification);
+
+        btnSendNotification.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                final String notification = edtNotification.getText().toString();
+                if(notification.trim().length() > 0) {
+                    sendNotificationToServer(notification, dialog);
+                } else {
+                    Toast.makeText(HomeActivity.this,
+                            "Por favor ingrese una notificación",
+                            Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+        dialog.show();
+    }
+
+    private void sendNotificationToServer(String notification, final Dialog dialog) {
+        JSONObject data = new JSONObject();
+        try {
+            data.put("description", notification);
+            JsonObjectRequest request = new JsonObjectRequest(Request.Method.POST,
+                    api_url + "/notification", data,
+                    new Response.Listener<JSONObject>() {
+                        @Override
+                        public void onResponse(JSONObject response) {
+                            dialog.dismiss();
+                            Toast.makeText(HomeActivity.this, "Notificación enviada", Toast.LENGTH_SHORT).show();
+                        }
+                    }, new Response.ErrorListener() {
+                @Override
+                public void onErrorResponse(VolleyError error) {
+
+                }
+            }){
+                @Override
+                public Map<String, String> getHeaders() throws AuthFailureError {
+                    HashMap<String, String> headers = new HashMap<>();
+                    headers.put("Authorization", "bearer " + Auth.getAuth(HomeActivity.this).getToken() );
+                    return headers;
+                }
+            };
+            VolleyS.getInstance(this).getQueue().add(request);
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
     }
 
     public void openFragment(Fragment fragment) {
