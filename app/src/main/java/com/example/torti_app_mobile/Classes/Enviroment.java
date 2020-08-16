9999bb989ac4b563dd50004b207a34bbb70d44e2
marru <marrufojosecarlos@gmail.com>
2020-08-16 16:30:34 -0500

    Dialog for description in devolution history was added ):

--- app/src/main/java/com/example/torti_app_mobile/Classes/Enviroment.java
+++ app/src/main/java/com/example/torti_app_mobile/Classes/Enviroment.java
@@ -2,6 +2,6 @@ package com.example.torti_app_mobile.Classes;
 
 public class Enviroment {
     private static String protocolHTTP = "http://";
-    private static String host = "ec2-54-198-29-169.compute-1.amazonaws.com/api/deliverier";
+    private static String host = "192.168.0.14:3333/api/deliverier";//"ec2-54-198-29-169.compute-1.amazonaws.com/api/deliverier";
     public static String api_url = protocolHTTP+host;
 }
